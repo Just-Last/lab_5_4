f761acd51be0274af5580694d49a62df22e50e25
Just-Last <nanaggmm18@gmail.com>
2022-03-30 11:33:07 +0300

    Initial commit

---
+++ src/lab_5_4.java
@@ -0,0 +1,5 @@
+public class lab_5_4 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Батько: Моздір Роман Миколаєвич \nМати: Моздір Оксана Аркадіївна \nБрат: Моздір Станіслав Романович");
+    }
+}
